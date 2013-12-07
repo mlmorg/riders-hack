86c0296cb9df31689552036f665efac6953e962f
Matt Morgan <mlmorg@gmail.com>
2013-12-07 01:01:32 -0800

    Add transition to detail page

--- main.js
+++ main.js
@@ -3,9 +3,15 @@
   var tileUrl = 'http://a.tiles.mapbox.com/v3/mlmorg.gfnol46k/{z}/{x}/{y}.png';
   var iconUrl = 'http://a.tiles.mapbox.com/v3/marker/pin-l-{text}+{color}.png';
 
-  function Riders (el, options) {
-    this.leaflet = L.map(el);
+  function Riders () {
+    this.map = document.getElementById('map');
     this.summary = document.getElementById('summary');
+    this.mapScreen = document.getElementById('map-screen');
+    this.detailScreen = document.getElementById('detail-screen');
+
+    this.leaflet = L.map(this.map);
+
+    this.summary.addEventListener('click', this.showDetail.bind(this));
 
     L.tileLayer(tileUrl).addTo(this.leaflet);
     this.leaflet.setView([-29.609988, 28.233608], 8);
@@ -25,6 +31,11 @@
     if (!this.summary.className.match(/show/)) {
       this.summary.className += ' show';
     }
+  };
+
+  Riders.prototype.showDetail = function () {
+    this.detailScreen.className = this.detailScreen.className.replace('out-right', '');
+    this.mapScreen.className += ' out-left';
   };
 
   Riders.prototype.addMarker = function (latlng, options) {
@@ -53,7 +64,6 @@
     return L.icon(options);
   };
 
-  var el = document.getElementById('map');
-  new Riders(el);
+  new Riders();
 
 })();
